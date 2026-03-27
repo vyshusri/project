ee3e7250ed050a75e032bc2a75b5ec5d39370601
vyshusri <vyshusriram15@gmail.com>
2026-03-27 10:05:48 -0700

    phone.java

---
+++ Sriram.java
@@ -0,0 +1,8 @@
+public class Sriram{
+    public static void main(Sring[] Args){
+        System.out.println("Device name:realme 11 Pro+5G");
+        System.out.println("RAM:8.00 GB");
+        System.out.print("Display:17.02");
+        System.out.println("model:gsdh");
+             }
+}
